283c563751785fbe768cfa22103fc3709444f530
Thiago Peixoto <thiago.peixots@gmail.com>
2025-09-10 15:37:21 -0300

    Camada Domain

---
+++ src/Domain/Entity/RefreshToken.cs
@@ -0,0 +1,13 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Core.Entity;
+
+public class RefreshToken
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public Guid UserId { get; set; }
+    public string TokenHash { get; set; }
+    public DateTime CreatedAt => DateTime.UtcNow;
+    public DateTime ExpiresAt => DateTime.UtcNow.AddHours(2);
+    public bool? Revoked;
+}
---
+++ src/Domain/Entity/User.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entity;
+
+public class User
+{
+    public User()
+    {
+
+    }
+    [Key]
+    public Guid Id { get; private set; } = Guid.NewGuid();
+    public string Username { get; set; } = string.Empty;
+    [EmailAddress]
+    public string Email { get; set; } = string.Empty;
+    public string Passwordhash { get; set; } = string.Empty;
+    public bool EmailConfirmed { get; set; }
+    public DateTime CreatedAt => DateTime.Now;
+    public DateTime UpdatedAt { get; set; }
+    public int FailedLoginAttempts
+    {
+        get { return FailedLoginAttempts; }
+        set
+        {
+            if (FailedLoginAttempts >= 5) throw new ArgumentException("Você excedeu as tentativas de login");
+        }
+    }
+
+    public User (Guid id, string username, string email, string passwordhash)
+    {
+        Id = id;
+        Username = username;
+        Email = email;
+        Passwordhash = passwordhash;
+    }
+
+}
