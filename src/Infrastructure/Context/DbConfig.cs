285383fba11875f36a2d0d7d2dc9c3eb557edc31
Thiago Peixoto <thiago.peixots@gmail.com>
2025-09-10 15:37:33 -0300

    Camada do banco

---
+++ src/Infrastructure/Context/DbConfig.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Context;
+public class DbConfig:DbContext
+{
+    public DbConfig(DbContextOptions<DbConfig> options) : base(options) { }
+    public DbSet<User> Users { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
