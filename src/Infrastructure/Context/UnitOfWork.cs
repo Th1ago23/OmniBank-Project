0f3a1d72e7f97f66a28fe1c84521e006c7a5d413
Thiago Peixoto dos Santos <thiago.santos.ts3@csn.com.br>
2025-09-15 14:58:30 -0300

    Criei o unit of work

---
+++ src/Infrastructure/Context/UnitOfWork.cs
@@ -0,0 +1,39 @@
+﻿using Domain.Interface.Repository;
+using Infrastructure.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Context
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DbConfig _context;
+
+        public IUserRepository Users { get; }
+        public IAttemptsRepository Attempts { get; }
+        public IRefreshTokenRepository Tokens { get; }
+        public IRoleRepository Roles { get; }
+        public IAdressRepository Adresses { get; }
+
+        public UnitOfWork(DbConfig context)
+        {
+            _context = context;
+            Users = new UserRepository(_context);
+            Tokens = new TokenRepository(_context);
+            Attempts = new AttemptRepository(_context);
+            Expenses = new ExpenseRepository(_context);
+        }
+        public async Task<int> CommitAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
