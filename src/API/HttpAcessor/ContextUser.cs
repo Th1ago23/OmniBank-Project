8708a462f2ef36a78edd06f35ab22ce6b4ce0d87
Thiago Peixoto dos Santos <thiago.santos.ts3@csn.com.br>
2025-09-17 14:47:42 -0300

    Criei o acessor de usuario em contexto

---
+++ src/API/HttpAcessor/ContextUser.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Interface.Http;
+using System.Security.Claims;
+
+namespace API.HttpAcessor;
+
+public class ContextUser(IHttpContextAccessor acessor) : IContextUser
+{
+    private readonly IHttpContextAccessor _acessor = acessor;
+
+    public Guid? UserId
+    {
+        get
+        {
+            var userIdString = _acessor.HttpContext?.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            return string.IsNullOrEmpty(userIdString) ? null : Guid.Parse(userIdString);
+        }
+    }
+    public string? UserEmail => _acessor.HttpContext?.User?.FindFirst(ClaimTypes.Email)?.Value;
+}
