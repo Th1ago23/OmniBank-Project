0f7566f2bdfb50a46894d243f5178661af232fa4
Thiago Peixoto dos Santos <thiago.santos.ts3@csn.com.br>
2025-09-17 14:47:59 -0300

    Novas interfaces

---
+++ src/Application/Interface/UserService/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using Application.DTO.Users;
+using Shared;
+
+namespace Application.Interface.UserService;
+
+public interface IAuthService
+{
+    public Task<UserResponseDTO> Login(UserLoginDTO login);
+    public Task<bool> Logout();
+    public Task<UserResponseDTO> Register(UserRegisterDTO register);
+}
---
+++ src/Application/Interface/UserService/IUserMapper.cs
@@ -0,0 +1,12 @@
+﻿using Application.DTO.Users;
+using Domain.Entity;
+
+namespace Application.Interface.UserService;
+
+public interface IUserMapper
+{
+    public User ToEntity(UserLoginDTO login);
+    public User ToEntity(UserRegisterDTO register);
+    public UserResponseDTO ToResponse(UserLoginDTO login);
+    public UserLoginDTO ToResponse(UserRegisterDTO register);
+}
