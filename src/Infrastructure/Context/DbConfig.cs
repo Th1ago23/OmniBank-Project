500bb9e64af83d10c2f225ff90a45b8cd24b3968
Thiago Peixoto dos Santos <thiago.santos.ts3@csn.com.br>
2025-09-17 14:49:48 -0300

    Novas config do banco *necessario dar nova migration

--- src/Infrastructure/Context/DbConfig.cs
+++ src/Infrastructure/Context/DbConfig.cs
@@ -1,6 +1,7 @@
 ﻿using Core.Entity;
 using Domain.Entity;
 using Microsoft.EntityFrameworkCore;
+using System.Security;
 
 namespace Infrastructure.Context;
 
@@ -17,6 +18,38 @@ public class DbConfig : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
+
+        var adminRoleId = Guid.NewGuid();
+        var analystRoleId = Guid.NewGuid();
+        var managerRoleId = Guid.NewGuid();
+        var userRoleId = Guid.NewGuid();
+        var supportRoleId = Guid.NewGuid();
+
+        modelBuilder.Entity<Role>().HasData(
+            new Role { Id = adminRoleId, Name = RolesName.Admin, Description = "Administrador total" },
+            new Role { Id = analystRoleId, Name = RolesName.Analyst, Description = "Analista financeiro" },
+            new Role { Id = managerRoleId, Name = RolesName.Manager, Description = "Gerente" },
+            new Role { Id = userRoleId, Name = RolesName.User, Description = "Usuário padrão" },
+            new Role { Id = supportRoleId, Name = RolesName.Support, Description = "Suporte ao cliente" }
+        );
+
+        var createUserPermId = Guid.NewGuid();
+        var editUserPermId = Guid.NewGuid();
+        var deactivateUserPermId = Guid.NewGuid();
+        var viewLogsPermId = Guid.NewGuid();
+        var createReportPermId = Guid.NewGuid();
+        var runQueryPermId = Guid.NewGuid();
+        var approveReportPermId = Guid.NewGuid();
+
+        modelBuilder.Entity<Permission>().HasData(
+            new Permission { Id = createUserPermId, Name = "CreateUser", Description = "Pode criar usuários" },
+            new Permission { Id = editUserPermId, Name = "EditUser", Description = "Pode editar usuários" },
+            new Permission { Id = deactivateUserPermId, Name = "DeactivateUser", Description = "Pode desativar usuários" },
+            new Permission { Id = viewLogsPermId, Name = "ViewLogs", Description = "Pode acessar logs e auditoria" },
+            new Permission { Id = createReportPermId, Name = "CreateReports", Description = "Pode criar relatórios" },
+            new Permission { Id = runQueryPermId, Name = "RunQueries", Description = "Pode rodar consultas" },
+            new Permission { Id = approveReportPermId, Name = "ApproveReports", Description = "Pode aprovar relatórios" }
+        );
 
         modelBuilder.Entity<User>(entity =>
         {
@@ -40,6 +73,11 @@ public class DbConfig : DbContext
                   .HasForeignKey(i=> i.UserId)
                   .OnDelete(DeleteBehavior.Cascade);
         });
+
+        modelBuilder.Entity<Role>()
+                  .HasMany(r => r.Permissions)
+                  .WithMany(p => p.Roles)
+                  .UsingEntity(j => j.ToTable("RolePermissions"));
 
         modelBuilder.Entity<Role>(entity =>
         {
