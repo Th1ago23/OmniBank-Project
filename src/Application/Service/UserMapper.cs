f3332e257a359b89a8e1678237c15a4e6549afdc
Thiago Peixoto dos Santos <thiago.santos.ts3@csn.com.br>
2025-09-17 14:48:43 -0300

    iniciei as classes de handles

---
+++ src/Application/Service/UserMapper.cs
@@ -0,0 +1,17 @@
+﻿using Application.DTO.Users;
+using Application.Interface.UserService;
+using Domain.Entity;
+
+namespace Application.Service;
+
+public class UserMapper:IUserMapper
+{
+    public User ToEntity(UserLoginDTO userLoginDTO)
+    {
+        return new User
+        {
+            Email = userLoginDTO.email,
+
+        }
+    }
+}
